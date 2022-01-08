678cacb05047c52a5f34a278a35d070c2b5ba370
Nicole Ashlyn Karas <nkaras@uwo.ca>
2022-01-08 15:00:37 -0500

    add finish profile for onboarding

--- assets/scripts/form-submission.js
+++ assets/scripts/form-submission.js
@@ -1,7 +1,19 @@
 function logSubmitStepOne(event) {
-    alert(`Form Submitted!`);
+    var formOne = document.getElementsByClassName("onboarding-form")[0];
+    formOne.classList.add("display-none");
+
+    var formTwo = document.getElementsByClassName("onboarding-form-2")[0];
+    formTwo.classList.remove("display-none");
+    event.preventDefault();
+  }
+
+function logSubmitStepTwo(event) {
+    alert("DONE");
     event.preventDefault();
   }
   
-  var form = document.getElementById('formOne');
-  form.addEventListener('submit', logSubmitStepOne);
+  var formOne = document.getElementById('formOne');
+  formOne.addEventListener('submit', logSubmitStepOne);
+
+  var formTwo = document.getElementById('formTwo');
+  formTwo.addEventListener('submit', logSubmitStepTwo);
